c54d9b756fc56b401185eead316a6374d6db1c4d
Alan Adamiak <alan.adamiak@u-psud.fr>
2021-04-02 15:59:30 +0200

    first commit

---
+++ Assets/_Scripts/ChatController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatController : MonoBehaviour
+{
+    public Button sendButton;
+    public InputField inputField;
+    public TMP_Text chat;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sendButton.onClick.AddListener(SendMessage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SendMessage()
+    {
+        if (inputField.text != "")
+        {
+            if (Random.value < 0.5)
+            {
+                chat.text += "<color=green><b>John</b>: ";
+                chat.text += inputField.text + "</color>\n\n";
+            }
+            else
+            {
+                chat.text += "<color=purple><b>You</b>: ";
+                chat.text += inputField.text + "</color>\n\n";
+            }
+
+            inputField.text = "";
+        }
+    }
+}
